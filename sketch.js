78b63a24d43f889d2ea245b6e3ae49e8d6289fb3
kwong09 <wongkayla09@gmail.com>
2025-09-22 15:50:40 -0700

    created framework for main plaza and starting screen

--- sketch.js
+++ sketch.js
@@ -5,11 +5,20 @@
 
 let spinningShape;
 let highScore = 0;
+let screen = "home";
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
+    createCanvas(800, 500);
 
     world.gravity.y = 10;
+
+    // home page sprites
+
+    startButton = new Sprite(-500, -500, 150, 50);
+    startButton.text = "start";
+    startButton.collider = "static";
+    startButton.fill = "pink";
+    /* STARTING CODE!!
 
     // this sprite can be created on a single line, but it's easier to read this way:
 	spinningShape = new Sprite();
@@ -18,9 +27,71 @@ function setup() {
     spinningShape.collider = "kinematic";
 
     textFont("Courier", 24);
+
+    */
 }
 
 function draw() {
+    clear();
+
+    if (screen == "home") {
+        background('pink');
+
+        // position start button
+        startButton.x = width / 2;
+        startButton.y = 100;
+
+        // changes cursor
+        cursorHovering(startButton);
+
+        if (startButton.mouse.presses()) {
+            screen = "mainPlaza";
+            startButton.pos = {x: -500, y: -500};
+
+            mainPlazaScreen();
+        }
+    }
+
+    if (screen == "mainPlaza") {
+        // text("bunny x coordinate: " + bunny.x, 50, 50);
+        background('lightblue');
+        mouse.cursor = 'default';
+        text("X Value: " + bunny.x, 50, 50);
+
+        // cursor hovering
+
+        // camera follows bunny
+        camera.x = bunny.x + 100;
+        ground.x = camera.x; 
+
+
+        //bunny movement
+        if (kb.pressing('right') || kb.pressing('d')) {
+            bunny.vel.x = 3;
+        } else if (kb.pressing('left') || kb.pressing('a')) {
+            bunny.vel.x = -3;
+        } else if (!keyIsPressed) {
+            bunny.vel.x = 0;
+        }
+
+        if (bunny.x >= 750 && bunny.x <= 800) {
+            enterOffice.x = bunny.x + 100;
+            enterOffice.y = bunny.y + 125;
+        } else {
+            enterOffice.x = -500;
+            enterOffice.y = -500;
+        }
+    }
+
+    function cursorHovering(object) {
+        if (object.mouse.hovering()) {
+            mouse.cursor = 'pointer';
+        } else {
+            mouse.cursor = 'default';
+        }
+    }
+
+    /* STARTING CODE!!
 
     // try the game without this line :)
     clear();
@@ -31,7 +102,7 @@ function draw() {
 
     // create a ball when the mouse is clicked
     // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
+    if (kb.pressing('up')) {
         let ball = new Sprite(mouse.x, -20, 20);
         // make the ball resist rolling when it touches the spinning shape
         // https://p5play.org/learn/sprite.html?page=9
@@ -60,5 +131,37 @@ function draw() {
     // display the score (minus 1 so it doesn't count the spinning shape)
     text("BALLS: " + (currentScore-1), 40, 60);
     text("HIGH:  " + (highScore-1), 40, 86);
+
+
+    */
+
+}
+
+
+function mainPlazaScreen() {
+
+    noStroke();
+
+    // main plaza sprites
+    ground = new Sprite(width / 2, height - 60, 800, 120);
+    ground.collider = 's';
+    ground.color = "lightgreen";
+    ground.friction = 0;
+
+    bunny = new Sprite(200, 300, 100, 125);
+    bunny.collider = 'd';
+    bunny.color = "white";
+    bunny.rotationLock = true;
+
+    postOffice = new Sprite(900, 230, 600, 300);
+    postOffice.collider = 'n';
+    postOffice.layer = 1;
+
+    enterOffice = new Sprite(-500, -500, 200, 50);
+    enterOffice.collider = 'n';
+    enterOffice.color = "white";
+    enterOffice.layer = 4;
+    enterOffice.text = 'Enter Post Office';
+
 
 }
