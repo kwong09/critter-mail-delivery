cc7c40b39bd6c04e24ea8516fba4281831023105
kwong09 <wongkayla09@gmail.com>
2025-10-19 17:14:11 -0700

    added animations and post office screen

--- sketch.js
+++ sketch.js
@@ -18,17 +18,12 @@ function setup() {
     startButton.text = "start";
     startButton.collider = "static";
     startButton.fill = "pink";
-    /* STARTING CODE!!
 
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
-
-    */
+    //load animations
+    bunnyRight = loadAni('/assets/bunnyRight.gif');
+    bunnyRight.scale = 0.25;
+    bunnyStill = loadAni('/assets/bunnyStill.png');
+    bunnyStill.scale = 0.25;
 }
 
 function draw() {
@@ -48,18 +43,14 @@ function draw() {
             screen = "mainPlaza";
             startButton.pos = {x: -500, y: -500};
 
-            mainPlazaScreen();
+            mainPlazaScreen("open");
         }
     }
 
     if (screen == "mainPlaza") {
-        // text("bunny x coordinate: " + bunny.x, 50, 50);
         background('lightblue');
         mouse.cursor = 'default';
         text("X Value: " + bunny.x, 50, 50);
-
-        // cursor hovering
-
         // camera follows bunny
         camera.x = bunny.x + 100;
         ground.x = camera.x; 
@@ -68,100 +59,144 @@ function draw() {
         //bunny movement
         if (kb.pressing('right') || kb.pressing('d')) {
             bunny.vel.x = 3;
+            bunny.changeAni('right');
         } else if (kb.pressing('left') || kb.pressing('a')) {
             bunny.vel.x = -3;
         } else if (!keyIsPressed) {
             bunny.vel.x = 0;
+            bunny.changeAni('still');
         }
 
         if (bunny.x >= 750 && bunny.x <= 800) {
-            enterOffice.x = bunny.x + 100;
-            enterOffice.y = bunny.y + 125;
+            enteringOffice = true;
+            enterOffice.x = 775;
+            enterOffice.y = 200;
+            cursorHovering(enterOffice)
+
         } else {
             enterOffice.x = -500;
             enterOffice.y = -500;
-        }
-    }
-
-    function cursorHovering(object) {
-        if (object.mouse.hovering()) {
-            mouse.cursor = 'pointer';
-        } else {
             mouse.cursor = 'default';
         }
-    }
 
-    /* STARTING CODE!!
+        if (bunny.x <= 110) {
+            bunny.x = 110;
+        }
 
-    // try the game without this line :)
-    clear();
-
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (kb.pressing('up')) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
-    }
-
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
-
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
+        if (enterOffice.mouse.presses()) {
+            mainPlazaScreen("close");
+            screen = "postOffice";
+            camera.x = 0;
+            camera.y = 0;
+            postOfficeScreen("open");
         }
     }
 
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
+    if (screen == "postOffice") {
+        background("lightyellow");
+        menuBar.draw();
+        worldMap.draw();
+        shop.draw();
+        mail.draw();
+        mouse.cursor = 'default';
 
+        if (worldMap.mouse.presses()) {
+            screen = "worldMap";
+        }
 
-    */
+    }
+
+    if (screen == "worldMap") {
+
+    }
+
+    if (screen == "forestMap") {
+
+    }
+
+    if (screen == "underwaterMap") {
+
+    }
+
+    if (screen == "desertMap") {
+
+    }
 
 }
 
 
-function mainPlazaScreen() {
+function cursorHovering(object) {
+    if (object.mouse.hovering()) {
+        mouse.cursor = 'pointer';
+    } else {
+        mouse.cursor = 'default';
+    }
+}
+
+
+function mainPlazaScreen(openClose) {
 
     noStroke();
 
-    // main plaza sprites
-    ground = new Sprite(width / 2, height - 60, 800, 120);
-    ground.collider = 's';
-    ground.color = "lightgreen";
-    ground.friction = 0;
+    if (openClose == "open") {
 
-    bunny = new Sprite(200, 300, 100, 125);
-    bunny.collider = 'd';
-    bunny.color = "white";
-    bunny.rotationLock = true;
+        // main plaza sprites
+        ground = new Sprite(width / 2, height - 60, 800, 120);
+        ground.collider = 's';
+        ground.color = "lightgreen";
+        ground.friction = 0;
 
-    postOffice = new Sprite(900, 230, 600, 300);
-    postOffice.collider = 'n';
-    postOffice.layer = 1;
+        bunny = new Sprite(200, 300, 100, 125);
+        bunny.collider = 'd';
+        bunny.color = "white";
+        bunny.rotationLock = true;
+        bunny.addAni('right', bunnyRight);
+        bunny.addAni('still', bunnyStill);
 
-    enterOffice = new Sprite(-500, -500, 200, 50);
-    enterOffice.collider = 'n';
-    enterOffice.color = "white";
-    enterOffice.layer = 4;
-    enterOffice.text = 'Enter Post Office';
+        postOffice = new Sprite(900, 230, 600, 300);
+        postOffice.collider = 'n';
+        postOffice.layer = 1;
+
+        enterOffice = new Sprite(-500, -500, 200, 50);
+        enterOffice.collider = 's';
+        enterOffice.color = "white";
+        enterOffice.layer = 4;
+        enterOffice.text = 'Enter Post Office';
 
 
+    } else if (openClose == "close") {
+        ground.pos = {x: -1000, y: -1000};
+        bunny.pos = {x: -1000, y: -1000};
+        postOffice.pos = {x: -1000, y: -1000};
+        enterOffice.pos = {x: -1000, y: -1000};
+    }
+
+
+}
+
+function postOfficeScreen(openClose) {
+    noStroke();
+
+    if (openClose == "open") {
+        menuBar = new Sprite(400, 450, 800, 200)
+        menuBar.collider = 's';
+        menuBar.color = "lightgreen";
+        menuBar.friction = 0;
+        menuBar.layer = -1;
+
+        worldMap = new Sprite(250, 425, 100, 100);
+        worldMap.collider = 's';
+        worldMap.color = "pink";
+        worldMap.layer = 4;
+
+        shop = new Sprite(400, 425, 100, 100);
+        shop.collider = 's';
+        shop.color = "lavender";
+        shop.layer = 4;
+
+        mail = new Sprite(550, 425, 100, 100);
+        mail.collider = 's';
+        mail.color = "gray";
+        mail.layer = 4;
+    }
 }
